81f5da3d098cc61371d0a5d5219c9a5cf5026849
didulanthaisuru <isurudidulantha@gmail.com>
2025-08-12 12:34:33 +0530

    create booking model

--- backend/models.py
+++ backend/models.py
@@ -3,6 +3,8 @@ from typing import Optional
 from datetime import datetime
 from uuid import uuid4
 from typing import List
+from typing import Dict
+from datetime import date, time
 
  
 class user(BaseModel):
@@ -14,9 +16,18 @@ class user(BaseModel):
     user_id: int
     email: str
 
-class Services(BaseModel):
+class services(BaseModel):
     service_name: str
     service_id: int
     main_service: str
     department_id: int
     required_documents: List[str]  # list of document names
+
+class Booking(BaseModel):
+    booking_id: int
+    service_id: int
+    doc_states: Dict[str, str]  # or Dict[str, bool] depending on your data
+    date: date
+    time: time
+    booking_state: str
+    predicted_duration: time
