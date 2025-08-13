b9f593821be05499b3434be1ac73b4602df85f29
Amandi Gunarathne <amandigunarathne55@gmail.com>
2025-08-13 14:04:42 +0530

    main fixed

--- backend/main.py
+++ backend/main.py
@@ -1,25 +1,6 @@
 from fastapi import FastAPI
-<<<<<<< HEAD
-from backend.routes.document import router as documents_router
-from backend.database_config import connect_to_mongo, close_mongo_connection
-
-app = FastAPI()
-
-app.include_router(documents_router)
-
-@app.get("/")
-async def root():
-    return {"message": "Document Upload Service"}
-
-@app.on_event("startup")
-async def startup_db_client():
-    await connect_to_mongo()
-
-@app.on_event("shutdown")
-async def shutdown_db_client():
-    await close_mongo_connection()
-=======
 from routes import appoinment
+from routes import document
 from database_config import connect_to_mongo, close_mongo_connection
 
 app = FastAPI()
@@ -37,4 +18,4 @@ def read_root():
     return {"message": "SmartGov API is running"}
 
 app.include_router(appoinment.router, prefix="/api", tags=["Appointments"])
->>>>>>> 3081573dc5c2b19c6461ddfe6f563bfed7b61e6c
+app.include_router(document.router, prefix="/api/documents", tags=["Documents"])
