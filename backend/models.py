c3f3a62851d5d33b5b56679c9b52551eb4850099
Amandi Gunarathne <amandigunarathne55@gmail.com>
2025-08-13 13:46:51 +0530

    uploadedDocument  model added

--- backend/models.py
+++ backend/models.py
@@ -34,6 +34,16 @@ class booking(BaseModel):
     predicted_duration: time
 
 
+class UploadedDocument(BaseModel):
+    booking_id: int
+    doc_id: int
+    required_doc_id: Optional[int] = None
+    file_name: str
+    file_path: str
+    accuracy: Optional[float] = None
+    doc_status: str = "pending"
+    uploaded_at: datetime = Field(default_factory=datetime.now)
+   
 class Appointment(BaseModel):
     appointment_id: Optional[int] = Field(default=None, description="Unique identifier for the appointment")
     user_id: int = Field(..., description="ID of the user making the appointment")
@@ -44,4 +54,4 @@ class Appointment(BaseModel):
     is_fully_complered: Optional[bool] = Field(..., description="Flag indicating if the appointment is fully completed")
     appointment_date: Optional[datetime] = Field(..., description="Date and time of the appointment")
     appoinment_time: Optional[datetime] = Field(..., description="Time of the appointment")
-    predicted_duration: Optional[datetime] = Field(..., description="Predicted duration of the appointment")  
+    predicted_duration: Optional[datetime] = Field(..., description="Predicted duration of the appointment")
