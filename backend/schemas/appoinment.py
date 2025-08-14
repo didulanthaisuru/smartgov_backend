de3f0910fd3748450b7f24f7299f0740e9ad5795
Nithila Thawalampitiya <nithilanimraka@gmail.com>
2025-08-14 16:29:21 +0530

    null default to datetime appointmentAdd

--- backend/schemas/appoinment.py
+++ backend/schemas/appoinment.py
@@ -8,9 +8,7 @@ class AppointmentAdd(BaseModel):
     sub_service_id: str = Field(..., description="ID of the service being booked")  # <-- Change to str
     created_at: Optional[datetime] = Field(default_factory=datetime.now, description="Timestamp when the appointment was created")
     is_fully_completed: Optional[bool] = Field(..., description="Flag indicating if the appointment is fully completed")
-    appointment_date: Optional[datetime] = Field(..., description="Date and time of the appointment")
-    appoinment_time: Optional[datetime] = Field(..., description="Time of the appointment")
-    predicted_duration: Optional[datetime] = Field(..., description="Predicted duration of the appointment")
+    appointment_date: Optional[datetime] = Field(default=None, description="Date and time of the appointment")
+    appoinment_time: Optional[datetime] = Field(default=None, description="Time of the appointment")
+    predicted_duration: Optional[datetime] = Field(default=None, description="Predicted duration of the appointment")
     payment_status: Optional[bool] = Field(default=False, description="Payment status of the appointment")
-
-
