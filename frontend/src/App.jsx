ff8826c156bafe86650cc4069f8e561d45aa8220
Thamindu17 <166316896+Thamindu17@users.noreply.github.com>
2025-08-14 13:19:06 +0530

    new

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -320,6 +320,7 @@ function App() {
                   <AdminPage />
                 // </ProtectedRoute>
               } 
+              
             />
             
             {/* Additional routes */}
